ae8f2ffb508366b2735b14dbb2599450ddc1d64c
Rahul Kumar <213158789+rahulkumar1311@users.noreply.github.com>
2026-08-01 17:37:07 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (89.58%) - LeetSync

---
+++ 486-predict-the-winner/predict-the-winner.java
@@ -0,0 +1,29 @@
+class Solution {
+
+    Integer[][] dp;
+
+    public boolean predictTheWinner(int[] nums) {
+        int n = nums.length;
+        dp = new Integer[n][n];
+
+        return solve(nums, 0, n - 1) >= 0;
+    }
+
+    private int solve(int[] nums, int left, int right) {
+
+        if (left == right) {
+            return nums[left];
+        }
+
+        if (dp[left][right] != null) {
+            return dp[left][right];
+        }
+
+        int pickLeft = nums[left] - solve(nums, left + 1, right);
+        int pickRight = nums[right] - solve(nums, left, right - 1);
+
+        dp[left][right] = Math.max(pickLeft, pickRight);
+
+        return dp[left][right];
+    }
+}
